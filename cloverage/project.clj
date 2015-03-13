6df2f47ff3769170ae496d8f20ceffa4e5c86ab3
Russell Mull <russell.mull@puppetlabs.com>
2015-03-13 11:48:47 -0700

    Update to tools.cli v0.3.1
    This allows lein-cloverage to be used with other projects that depend
    on the newer version of tools.cli.

--- cloverage/project.clj
+++ cloverage/project.clj
@@ -11,7 +11,7 @@
             :distribution :repo
             :comments "same as Clojure"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.clojure/tools.cli "0.2.2"]
+                 [org.clojure/tools.cli "0.3.1"]
                  [org.clojure/tools.logging "0.2.3"]
                  [org.clojure/data.xml "0.0.7"]
                  [bultitude "0.2.0"]
